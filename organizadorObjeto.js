c83505a4a11a03497b0b820e801b18dc7fa97957
RafaelPatricio87 <87397552+RafaelPatricio87@users.noreply.github.com>
2023-02-02 03:49:23 -0300

    exercicio de logica

--- organizadorObjeto.js
+++ organizadorObjeto.js
@@ -1,20 +1,26 @@
 
 const dados = [
-    {itsMe: true,admin:true,name:'Rafael'},
-    {itsMe: false,admin:false,name:'Cris'},
-    {itsMe: false,admin:true,name:'Kainan'}
+    { itsMe: true, admin: false, name: 'thiel' },
+    { itsMe: true, admin: true, name: 'Rafael' },
+    { itsMe: false, admin: false, name: 'Cris' },
+    { itsMe: false, admin: true, name: 'Kainan' },
+    { itsMe: true, admin: true, name: 'nicollas' }
 ]
 
 const organizador = (dados) => {
-    let resultado= []
-    for(let index = 0; index < dados.length; index++){
-        if(dados[index].itsMe && dados[index].admin){
-               dados.unshift(dados.index)
-            
+    let newArray1 = [];
+    let newArray2= [];
+    let newArray3 = []
+    for (let index = 0; index < dados.length; index++) {
+        if (dados[index].itsMe && dados[index].admin) {
+            newArray1.push(dados[index])
+        }else if(!dados[index].itsMe && dados[index].admin){
+            newArray2.push(dados[index])
+        }else if(!dados[index].itsMe && !dados[index].admin){
+            newArray3.push(dados[index])
         }
-        console.log()
     }
-//  console.log(dados[0].itsMe)
-//  console.log(dados[0].admin)
+     
+     console.log(newArray1.concat(newArray2,newArray3))
 }
 organizador(dados)
